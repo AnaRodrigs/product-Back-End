bcb321e81ab9efd521a16287a232af4eb25a7e59
AnaRodrigs <mariaanarodrigues2005@gmail.com>
2023-08-22 17:00:19 -0300

    Put (update) completed

--- src/main/java/com/example/productbackend/resources/ProductController.java
+++ src/main/java/com/example/productbackend/resources/ProductController.java
@@ -3,13 +3,13 @@ package com.example.productbackend.resources;
 import java.net.URI;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,35 +18,35 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.productbackend.entities.Product;
 import com.example.productbackend.services.ProductService;
 
-
-
 @RestController
 @RequestMapping("products")
-public class ProductController{
+public class ProductController {
 
     @Autowired
-    private  ProductService service;
+    private ProductService service;
 
     @GetMapping
-    public ResponseEntity<List<Product>> getProducts(){
-         var products = this.service.getProducts();
-         return ResponseEntity.ok(products);
+    public ResponseEntity<List<Product>> getProducts() {
+        var products = this.service.getProducts();
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable long id){
+    public ResponseEntity<Product> getProduct(@PathVariable long id) {
         var product = this.service.getProduct(id);
         return ResponseEntity.ok(product);
-       
+
     }
+
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable long id){
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
         this.service.deleteProductById(id);
         return ResponseEntity.noContent().build();
-       
+
     }
+
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    public ResponseEntity<Product> save(@RequestBody Product product) {
         var saveProduct = this.service.save(product);
 
         URI location = ServletUriComponentsBuilder
@@ -54,7 +54,13 @@ public class ProductController{
                 .path("/{id}")
                 .buildAndExpand(saveProduct.getId())
                 .toUri();
-       return ResponseEntity.created(location).body(saveProduct);
-       
+        return ResponseEntity.created(location).body(saveProduct);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update( @RequestBody Product product, 
+                                        @PathVariable long id) {
+        this.service.update(id, product);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/productbackend/services/ProductService.java
+++ src/main/java/com/example/productbackend/services/ProductService.java
@@ -12,29 +12,44 @@ import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
-    
-@Autowired
-private ProductRepository repository;
 
-public List<Product> getProducts(){
-    return this.repository.findAll();
-}
+    @Autowired
+    private ProductRepository repository;
 
-public Product getProduct(long id){
-    return this.repository.findById(id)
-    .orElseThrow( () -> new EntityNotFoundException("Product not found "));
-                         
-}
-public void deleteProductById(long id){
-    if(this.repository.existsById(id)){
-        this.repository.deleteById(id);
-    }
-    else {
-        throw new EntityNotFoundException("Product not found ");
+    public List<Product> getProducts() {
+        return this.repository.findAll();
     }
 
+    public Product getProduct(long id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found "));
+
+    }
+
+    public void deleteProductById(long id) {
+        if (this.repository.existsById(id)) {
+            this.repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Product not found ");
+        }
+
+    }
+
+    public Product save(Product product) {
+        return this.repository.save(product);
+    }
+
+
+    public void update( long id, Product product) {
+        
+    try {
+        var updateProduct = this.repository.getReferenceById(id);
+        updateProduct.setName(product.getName());
+        updateProduct.setPrice(product.getPrice());
+        this.repository.save(updateProduct);
+    }
+   catch(EntityNotFoundException e){
+    throw new EntityNotFoundException("Product not found");
+   }
 }
- public Product save(Product product){
-    return this.repository.save(product);
- }
-}
+   }
